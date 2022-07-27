1f9e28bc4a3375cf5114d268918fa8890c6056bc
iAntopy <i-antony@hotmail.com>
2022-07-27 14:22:43 -0400

    mlx_set_bg_color.c now seting 2 pixel at a time -> twice as fast

--- mlx_addons/mlx_set_bg_color.c
+++ mlx_addons/mlx_set_bg_color.c
@@ -6,7 +6,7 @@
 /*   By: iamongeo <iamongeo@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/07/05 02:31:10 by iamongeo          #+#    #+#             */
-/*   Updated: 2022/07/05 02:40:37 by iamongeo         ###   ########.fr       */
+/*   Updated: 2022/07/27 08:24:19 by iamongeo         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,11 +14,28 @@
 
 void	mlx_set_bg_color(t_mlx *mlx, int col)
 {
-	int	*b;
+	size_t	*b;
+	int	*p;
 	int	nb_pxl;
+	size_t	xl;
 
+	p = (int *)&xl;
+	*p = col;
+	*(p + 1) = col;
+
+	b = (size_t*)mlx->off_buff->addr;
+	nb_pxl = mlx->width * mlx->height;
+	while (nb_pxl <= 2)
+	{
+		*(b++) = xl;
+		nb_pxl -= 2;
+	}
+	if (nb_pxl)
+		*(int *)b = col;
+/*
 	b = (int *)mlx->off_buff->addr;
 	nb_pxl = mlx->width * mlx->height;
 	while (nb_pxl--)
 		*(b++) = col;
+*/
 }
