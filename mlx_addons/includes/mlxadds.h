2a1375cc69146d71d118b93f72aff718aa665014
Ian Antony Mongeon Charest <i-antony@hotmail.com>
2022-08-20 08:52:10 -0400

    Updated header files to norminetted versions. mlx_render_buffers.c now includes 90% less printf's.

--- mlx_addons/includes/mlxadds.h
+++ mlx_addons/includes/mlxadds.h
@@ -6,7 +6,7 @@
 /*   By: iamongeo <iamongeo@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/07/04 17:32:17 by iamongeo          #+#    #+#             */
-/*   Updated: 2022/08/04 16:33:04 by iamongeo         ###   ########.fr       */
+/*   Updated: 2022/08/20 07:38:45 by iamongeo         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -28,19 +28,18 @@ enum	e_event_codes
 	ON_MOUSEMOVE = 6,
 	ON_EXPOSE = 12,
 	ON_DESTROY = 17
-
 };
 
-typedef struct	s_img_data
+typedef struct s_img_data
 {
 	void	*img;
 	char	*addr;
-	int	bytes_per_pxl;	//Don't forget to divide bits_per_pixel by 8.
-	int	line_len;
-	int	endian;	
+	int		bytes_per_pxl;
+	int		line_len;
+	int		endian;
 }	t_img;
 
-typedef struct	s_mlx_data
+typedef struct s_mlx_data
 {
 	void	*conn;
 	void	*win;
@@ -54,11 +53,11 @@ typedef struct	s_mlx_data
 	t_img	buff2;
 }	t_mlx;
 
-t_mlx	*mlx_init_double_buff_window(t_mlx *mlx, int width, int height, char *title);
-int	mlx_swap_buffers(t_mlx *mlx);
+t_mlx	*mlx_init_double_buff_window(t_mlx *mlx, int w, int h, char *title);
+int		mlx_swap_buffers(t_mlx *mlx);
 void	mlx_buff_put_pixel(t_img *img, int x, int y, int col);
 void	mlx_draw_line(t_mlx *mlx, int start[2], int end[2], int col);
-void	mlx_clear_buffer(t_mlx *mlx); //Clears the off screen buffer by setting its data to 0.
+void	mlx_clear_buffer(t_mlx *mlx);
 void	mlx_set_bg_color(t_mlx *mlx, int col);
 void	mlx_render_buffer(t_mlx *mlx);
 void	mlx_close(t_mlx *mlx);
--- mlx_addons/mlx_render_buffers.c
+++ mlx_addons/mlx_render_buffers.c
@@ -6,7 +6,7 @@
 /*   By: iamongeo <marvin@42quebec.com>             +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/07/22 23:04:27 by iamongeo          #+#    #+#             */
-/*   Updated: 2022/08/06 15:55:01 by iamongeo         ###   ########.fr       */
+/*   Updated: 2022/08/20 07:13:17 by iamongeo         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,11 +16,7 @@
 // pushes screen_buff to window and swaps buffers for next draw.
 void	mlx_render_buffer(t_mlx *mlx)
 {
-	printf("Render buffer starts\n");
 	mlx_swap_buffers(mlx);
-	printf("buffers swapped\n");
-	printf("Putting imag to window\n");
-	printf("conn ptr %p, win ptr %p, screen_buff ptr %p\n", mlx->conn, mlx->win, mlx->screen_buff);
-	if (mlx_put_image_to_window(mlx->conn, mlx->win, mlx->screen_buff->img, 0, 0) >= 0)
-		printf("Buffer rendered successfully\n");
+	if (mlx_put_image_to_window(mlx->conn, mlx->win, mlx->screen_buff->img, 0, 0) < 0)
+		printf("minilibx failed to render buffer to window\n");
 }
